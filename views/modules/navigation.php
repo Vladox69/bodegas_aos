5a08a39850f1b60dcdfb796d6b6ab73f287b4673
Vladox69 <marwladiagua2001@hotmail.com>
2021-07-18 06:01:46 -0500

    Nav luego de loguin

--- views/modules/navigation.php
+++ views/modules/navigation.php
@@ -1,27 +1,33 @@
 <header class="header ">
     <div class="contenedor contenido-header">
         <div class="barra">
-            <a href="index.php?action=login">
-                <img src="images/logotipo.png" alt="logotipo de bodegas">
-            </a>
-            <nav class="navegacion">
-                <a href="index.php?action=login"> Servicios</a>
-                <a href="index.php?action=sucursales"> Sucursales</a>
-                <a href="index.php?action=productos"> Productos</a>
-                <a href="index.php?action=contacto"> Contacto</a>
+
+            <?php
+            session_start();
+            if (isset($_SESSION['nom'])) {
+            ?>
+                <a href="index.php?action=sucursales">
+                    <img src="images/logotipo.png" alt="logotipo de bodegas">
+                </a>
+                <nav class="navegacion">
+                    <a href="index.php?action=sucursales"> Sucursales</a>
+                    <a href="index.php?action=productos"> Productos</a>
+                    <a href="index.php?action=contacto"> Contacto</a>
+                    <a href="models/logout.php"> Cerrar Sesión</a>
                 <?php
-                session_start();
-                if(isset($_SESSION['nom'])){
+            } else {
                 ?>
-                <a href="models/logout.php"> Cerrar Sesión</a>
-                <?php
-                }
-                ?>
-            </nav>
+                    <a href="index.php?action=login">
+                        <img src="images/logotipo.png" alt="logotipo de bodegas">
+                    </a>
+                    <nav class="navegacion">
+                        <a href="index.php?action=login"> Servicios</a>
+                        <a href="index.php?action=sucursales"> Sucursales</a>
+                        <a href="index.php?action=productos"> Productos</a>
+                        <a href="index.php?action=contacto"> Contacto</a>
+                    </nav>
+                <?php } ?>
         </div>
 
     </div>
 </header>
-
-
-
